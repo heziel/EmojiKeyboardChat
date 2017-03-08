6da3b0f8db32c5e9bdb4fc53f90885de7189ebad
heziel <hezi.eliyahu@gmail.com>
2017-03-08 02:31:06 -0800

    Initial Commit

---
+++ emoji-library/src/main/java/keyboard/emoji_library/Helper/EmojiconRecents.java
@@ -0,0 +1,13 @@
+package keyboard.emoji_library.Helper;
+
+import android.content.Context;
+
+import keyboard.emoji_library.Emoji.Emojicon;
+
+/**
+ * EmojiconRecents
+ */
+
+public interface EmojiconRecents {
+    public void addRecentEmoji(Context context, Emojicon emojicon);
+}
